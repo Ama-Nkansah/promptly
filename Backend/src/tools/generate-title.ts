42d50f23903c5552770538f888cfe4f51b9578de
samuelfreeman <samuelmanueljnr@gmail.com>
2025-10-26 21:46:03 +0000

    restructured project

---
+++ Backend/src/tools/generate-title.ts
@@ -0,0 +1,14 @@
+import { chatModel } from "../llm/ollama-client.js";
+
+
+export const generateTitle = async (topic: string, value?: number) => {
+    const prompt = `Generate a catchy and concise title for a blog post about : ${topic}`;
+
+    // value param to specify number of titles
+    if (value !== 0) {
+        const prompt = `Generate ${value} catchy and concise titles for a blog post about : ${topic}`;
+    return await chatModel(prompt);
+    }
+
+    return await chatModel(prompt);
+}
